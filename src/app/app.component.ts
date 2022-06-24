b7ea76d5c79e24ac54342ff16480b7ebb3a7ee03
Otoniel López <olopez@marnagarage.com>
2022-06-24 18:14:18 -0400

    clase 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,33 +7,6 @@ import { Product } from './models/product.model'
 })
 export class AppComponent {
   imgParent = '';
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: 'https://source.unsplash.com/random'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: 'https://source.unsplash.com/random'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: 'https://source.unsplash.com/random'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: 'https://source.unsplash.com/random'
-    },
-  ];
 
   onLoaded(url: string) {
     console.log('log padre' + url)
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model'
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://source.unsplash.com/random'
+    },
+  ];
 
   constructor() { }
 
