207a3669ab1dd2990fb8784d9579abf275742144
Otoniel López <olopez@marnagarage.com>
2022-06-24 17:28:30 -0400

    arreglar tipado de la carpeta components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './componets/img/img.component';
-import { ProductComponent } from './componets/product/product.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
     ProductComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
