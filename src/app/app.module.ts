682635ff3277538715c5b10a798cbe1e3a906e30
Otoniel López <olopez@marnagarage.com>
2022-06-24 16:26:55 -0400

    clase 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './componets/img/img.component';
+import { ProductComponent } from './componets/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
