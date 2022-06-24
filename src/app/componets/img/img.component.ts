b06f5809f7c7c4201db8f009130b0447a7cc2a69
Otoniel López <olopez@marnagarage.com>
2022-06-24 17:26:18 -0400

    clase 7

--- src/app/componets/img/img.component.ts
+++ src/app/componets/img/img.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
-
-  @Input() img: string = '';
+export class ImgComponent implements OnInit, OnChanges {
+  img: string = '';
+  @Input('img')
+  set changeImage(newImg: string) {
+    this.img = newImg
+  }
   @Output() loaded = new EventEmitter<string>();
   imageDefault: string = './assets/images/default.jpg';
 
   constructor() { }
+
+  ngOnChanges() {
+
+  }
 
   ngOnInit(): void {
   }
