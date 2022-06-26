9276b3aa7b574e3b41af0c10a0a22d3419ef46e6
Otoniel López <olopez@marnagarage.com>
2022-06-26 17:45:09 -0400

    clase 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import { NavComponent } from './components/nav/nav.component';
     ProductComponent,
     ProductsComponent,
     NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
