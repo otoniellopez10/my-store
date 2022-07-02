3c1e4479224a0d3d785dc1918ac53ab39eaad8ad
Otoniel López <olopez@marnagarage.com>
2022-07-02 00:30:47 -0400

    clase 12

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model'
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = '/api'
+  private apiUrl = `${environment.API_URL}/api`
 
   constructor(
     private http: HttpClient
