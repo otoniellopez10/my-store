8999b9d6009401573f110aacfce00fd8cae312b0
Otoniel López <olopez@marnagarage.com>
2022-07-04 17:15:40 -0400

    clase 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +11,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   imgParent = '';
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) { }
+
   onLoaded(url: string) {
     console.log('log padre' + url)
+  }
+
+  createUser() {
+    this.usersService.create({
+      name: 'Otoniel',
+      email: 'otoniellopez10@gmail.com',
+      password: '123'
+    })
+      .subscribe(rta => {
+        console.log(rta);
+      })
+  }
+
+  login() {
+    this.authService.login('otoniellopez10@gmail.com', '123')
+      .subscribe(rta => {
+        console.log(rta.access_token);
+      })
   }
 }
