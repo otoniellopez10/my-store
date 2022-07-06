d97394dae15a1bb1397ebf04b5a6747e21f30506
Otoniel López <olopez@marnagarage.com>
2022-07-05 23:20:58 -0400

    clase 20

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,6 +4,7 @@ import { retry, catchError, map } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model'
+import { checkTime } from '../interceptors/time.interceptor';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -24,7 +25,7 @@ export class ProductsService {
       params = params.set('limit', limit)
       params = params.set('offset', offset)
     }
-    return this.http.get<Product[]>(`${this.apiUrl}/products/`, { params })
+    return this.http.get<Product[]>(`${this.apiUrl}/products/`, { params, context: checkTime() })
       .pipe(
         map(products => products.map(item => {
           return {
