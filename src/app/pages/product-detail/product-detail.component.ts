5fdbbebfb5f74d08015c499df3bf301d3aaf0afa
Otoniel López <olopez@marnagarage.com>
2022-07-08 11:34:32 -0400

    clase 8: detalle de cada producto

---
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+// Services
+import { ProductsService } from 'src/app/services/products.service';
+
+// models
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  productId: string | null = null
+  product: Product | null = null
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        switchMap(params => {
+          this.productId = params.get('id')
+          if (this.productId) {
+            return this.productsService.getProduct(this.productId)
+          }
+          return []
+        })
+      )
+      .subscribe(data => {
+        this.product = data
+        console.log(data)
+      });
+  }
+
+  goToBack() {
+    this.location.back();
+  }
+
+}
